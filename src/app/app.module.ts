80b0d3ae77802c062aaff3912601f1cc2a1315b0
Victor de vitry <devitryv@gmail.com>
2023-01-12 15:46:44 +0100

    ajout avancements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageGestionComponent } from './page-gestion/page-gestion.component';
+import { PageListeComponent } from './page-liste/page-liste.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'liste', component: PageListeComponent },
+  { path: 'gestion', component: PageGestionComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component:HomeComponent}
+
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageGestionComponent,
+    PageListeComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    ReactiveFormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,    
+    RouterModule.forRoot(
+      appRoutes,
+    ),
+
+  ],
+ 
+  exports:[
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page-liste/page-liste.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-liste',
+  templateUrl: './page-liste.component.html',
+  styleUrls: ['./page-liste.component.css']
+})
+export class PageListeComponent {
+
+}
